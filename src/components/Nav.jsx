789f006517c660988380ed04ba20c483330635c6
MTM <marsoumtm@gmail.com>
2025-01-28 16:59:33 +0100

    sm-ch

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,11 @@
+import "../scss/components/nav.scss"
+
 const Nav = () => {
     return (
       <nav>
-        <div>MyLogo</div>
+        <div id="logo">MyLogo</div>
         
-        <ul>
+        <ul className="navLinks">
           <li><a href="#home">Blog</a></li>
           <li><a href="#about">Services</a></li>
           <li><a href="#services">Our Story</a></li>
